63cdadaee572ab0ef1f31128a913119c4532d366
Captsaiz <arkan.pradhana.heryanto@gmail.com>
2026-09-19 15:37:26 +0700

    base main.cpp created

---
+++ Songs-Asset/M.Sasuke - GG EZ.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> GG_EZ = {
+  ""
+};
+
+void M_Sasuke_GG_EZ() {
+
+}
--- main.cpp
+++ main.cpp
@@ -1,48 +1,35 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <filesystem>
 
-void tfm_upperStr(std::string& nama) {
-    for (char& c : nama) {
-        c = static_cast<char>(std::toupper(static_cast<unsigned char>(c)));
-    }
+namespace fs = std::filesystem;
+
+void errorMsg(const std::string& error) {
+	std::cerr << "ERROR: " << error << '\n';
 }
 
-void boom(const std::string& msg) {
-    while (true) {
-        std::cout << msg << '\n';
-    }
-}
-
-std::string inp_upr_string() {
-    std::string inp;
-    std::getline(std::cin, inp);
-    tfm_upperStr(inp);
-    return inp;
-}
-
-void scrMain(const std::string& usrOpsec) {
-    std::string ansOpsec;
-
-    std::cout << "Hey, don't say that!\n";
-    ansOpsec = inp_upr_string();
-    
+void tfm_upperStr(std::string& msg) {
+  for (char& c : msg) {
+  c = static_cast<char>(std::toupper(static_cast<unsigned char>(c)));
+  }
 }
 
 int main() {
-    std::string hitam;
-    std::string hitam_upr;
-    std::getline(std::cin, hitam);
-    hitam_upr = hitam;
-    tfm_upperStr(hitam_upr);
-    
-    if (hitam_upr == "GG FUGGING EZ") scrMain();
+  fs::path targetdir("./Songs-Asset");
+	if (!fs::exists(targetdir) || !fs::is_directory(targetdir)) {
+			errorMsg("Captsaiz, or anyone. You've fucked the Songs-Asset directionary");
+			return 1;
+	}
 
-    if (hitam_upr == "ATHALLA") boom("WUWU purple heart");
-    if (hitam_upr == "FADLAN") boom("otw di-doxx sama rio :3");
-    if (hitam_upr == "EZA") boom("yoda mati, twillight sparkle goated");
+	std::cout << "Welcome to Singing-Battle-Bullshit!!!\n\n";
+	std::cout << "Please select a song:\n";
 
-    std::cout << "Hello " << hitam << " :D" <<'\n'; 
+	for (const auto& entry : fs::directory_iterator(targetdir)) {
+		if (entry.is_regular_file()) {
+			std::cout << "\u00B7 " << entry.path().stem().string() << '\n';
+		}
+	}
 
-    return 0;
+	return 0;
 }
